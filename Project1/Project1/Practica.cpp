3f5b1d42835c8858f5bb830b7475318f6a2ff86a
Portatil <togirainoroka@gmail.com>
2017-09-25 09:57:44 +0200

    V1.1
    Base

---
+++ Project1/Project1/Practica.cpp
@@ -0,0 +1,24 @@
+//Practica Programacio II Contenidors i Iteradors
+
+//Includes
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+	//Cos de la practica
+	vector <int> vector1({ 1,2,3,4 });
+	// vector<int>::iterator it_vector_1;
+
+	for (vector<int>::iterator it_vector_1 = vector1.begin(); it_vector_1 != vector1.end(); it_vector_1++) {
+		cout << *it_vector_1 ;
+	}
+
+	for (vector<int>::reverse_iterator it_vector_2 = vector1.rbegin(); it_vector_2 != vector1.rend(); it_vector_2++) {
+		cout << *it_vector_2;
+	}
+}
